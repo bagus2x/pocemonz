767b2b036319aecc8f148f1491e89342c8491a0e
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-16 11:20:12 +0700

    add search feature

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import { Moon as MoonIcon, SearchNormal as SearchIcon, Sun1 as SunIcon } from 'iconsax-react'
-import { GithubIcon } from 'lucide-react'
+import { GithubIcon, Loader2Icon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 import { Button } from '@pocemonz/components/ui/button'
 import {
@@ -14,12 +15,48 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@pocemonz/components/ui/dropdown-menu'
+import { searchPokemon } from '@pocemonz/data/pokemon-api'
+import { useDebounce, useOutsideClick } from '@pocemonz/util/hooks'
 import { cn } from '@pocemonz/util/tw'
 import { PropsWithClassName } from '@pocemonz/util/types'
+import { useQuery } from '@tanstack/react-query'
+import { AnimatePresence } from 'framer-motion'
+import { useEffect, useMemo, useRef, useState } from 'react'
+
+const useSearchPokemons = ({ query }: { query: string }) => {
+  return useQuery({
+    queryKey: [`search-pokemon-${query}`, query],
+    queryFn: () => searchPokemon(query),
+    enabled: query.length !== 0,
+    select(results) {
+      return results.map((result) => ({
+        ...result,
+        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${result.id}.png`
+      }))
+    }
+  })
+}
 
 export const Navbar = ({ className }: PropsWithClassName) => {
   const { setTheme } = useTheme()
   const pathName = usePathname()
+  const [query, setQuery] = useState('')
+  const debouncedQuery = useDebounce(query, 250)
+  const { data, isLoading, isFetching } = useSearchPokemons({ query: debouncedQuery })
+  const slicedData = useMemo(() => data?.slice(0, 10) || [], [data])
+  const [searchBoxVisible, setSearchBoxVisible] = useState(false)
+  const searchInputRef = useRef<HTMLInputElement>(null)
+  const searchBoxRef = useRef<HTMLInputElement>(null)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useOutsideClick([searchInputRef, searchBoxRef], () => {
+    setSearchBoxVisible(false)
+  })
+
+  useEffect(() => {
+    setSearchBoxVisible(false)
+  }, [pathname, searchParams])
 
   return (
     <nav
@@ -36,11 +73,52 @@ export const Navbar = ({ className }: PropsWithClassName) => {
           <label htmlFor='search' className='sr-only'>
             Search pokemons
           </label>
-          <input
-            id='search'
-            className='w-full rounded-2xl bg-accent px-4 py-2 text-sm antialiased outline-none'
-            placeholder='Search pokemons'
-          />
+          <div className='relative w-full shadow-2xl'>
+            <input
+              id='search'
+              ref={searchInputRef}
+              value={query}
+              onChange={(ev) => setQuery(ev.target.value)}
+              className='w-full rounded-2xl bg-accent px-4 py-2 text-sm antialiased outline-none'
+              placeholder='Search pokemons'
+              onFocus={() => setSearchBoxVisible(true)}
+            />
+            <AnimatePresence>
+              {searchBoxVisible && !!debouncedQuery.length && (
+                <div
+                  className='absolute mt-4 w-full gap-4 rounded-2xl bg-background p-4'
+                  ref={searchBoxRef}>
+                  {slicedData.map(({ id, name, sprite }) => (
+                    <motion.div
+                      key={id}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className='flex cursor-pointer items-center gap-4 rounded-2xl p-2 transition-all hover:bg-accent'>
+                      <Link className='text-sm capitalize' href={`/pokemon/${name}`}>
+                        <Image
+                          width={40}
+                          height={40}
+                          src={sprite}
+                          alt={name}
+                          className='rounded-full'
+                        />
+                      </Link>
+                      <Link className='text-sm capitalize' href={`/pokemon/${name}`}>
+                        {name}
+                      </Link>
+                    </motion.div>
+                  ))}
+                  {!slicedData.length && debouncedQuery.length !== 0 && (
+                    <span className='p-4 text-sm'>No result found</span>
+                  )}
+                  <div className='flex items-center justify-center'>
+                    {(isFetching || isLoading) && <Loader2Icon className='animate-spin' />}
+                  </div>
+                </div>
+              )}
+            </AnimatePresence>
+          </div>
           <Button variant='ghost' size='icon' className='shrink-0 rounded-full bg-background/20'>
             <SearchIcon className='h-[1.2rem] w-[1.2rem]' />
           </Button>
--- src/data/pokemon-api.ts
+++ src/data/pokemon-api.ts
@@ -3,37 +3,50 @@ import { cache } from 'react'
 import { Page } from '@pocemonz/data/model/page'
 import { Pokemon } from '@pocemonz/data/model/pokemon'
 
-export const getPokemonNames = cache(async (page: number, size: number): Promise<Page<string>> => {
-  const res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * size}&limit=${size}`
-  )
-  if (!res.ok) {
-    throw new Error(await res.text())
-  }
+export const searchPokemon = async (query: string) => {
+  const { items: pokemons } = await getPokemonNames(1, 13000)
+  return pokemons.filter((pokemon) => pokemon.name.includes(query))
+}
 
-  const { count, next, results } = (await res.json()) as {
-    count: number
-    next: string | null
-    results: { name: string }[]
-  }
+export const getPokemonNames = cache(
+  async (page: number, size: number): Promise<Page<{ id: number; name: string }>> => {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * size}&limit=${size}`
+    )
+    if (!res.ok) {
+      throw new Error(await res.text())
+    }
 
-  const nextUrl = next ? new URL(next) : null
-  const nextPage = nextUrl
-    ? parseInt(new URLSearchParams(nextUrl.search).get('offset')!!) / size + 1
-    : page + 1
+    const { count, next, results } = (await res.json()) as {
+      count: number
+      next: string | null
+      results: { name: string; url: string }[]
+    }
 
-  return {
-    total: count,
-    size: size,
-    next: nextPage,
-    items: results.map((result) => result.name) || []
+    const nextUrl = next ? new URL(next) : null
+    const nextPage = nextUrl
+      ? parseInt(new URLSearchParams(nextUrl.search).get('offset')!!) / size + 1
+      : page + 1
+
+    return {
+      total: count,
+      size: size,
+      next: nextPage,
+      items:
+        results.map((result) => ({
+          id: parseInt(
+            result.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
+          ),
+          name: result.name
+        })) || []
+    }
   }
-})
+)
 
 export const getPokemons = cache(async (page: number, size: number): Promise<Page<Pokemon>> => {
   const { items: names, ...res } = await getPokemonNames(page, size)
 
-  const pokemons = await Promise.all(names.map((name) => getPokemon(name)))
+  const pokemons = await Promise.all(names.map(({ name }) => getPokemon(name)))
 
   return {
     ...res,
