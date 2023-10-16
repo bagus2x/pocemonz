2e8930a3d2798484258756e2709cf19961f70506
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-16 14:35:15 +0700

    add blurry background

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,7 @@
 import { PokemonList } from '@pocemonz/components/pokemon-list'
 import { AnimatableMain } from '@pocemonz/components/ui/animatable-element'
 import { getPokemons } from '@pocemonz/data/pokemon-api'
+import { getPokemonTypes } from '@pocemonz/data/type'
 
 interface DashboardPageProps {
   searchParams: {
@@ -11,6 +12,7 @@ interface DashboardPageProps {
 export default async function DashboardPage({ searchParams }: DashboardPageProps) {
   const page = parseInt(searchParams.page || '1') || 1
   const pokemons = await getPokemons(page, 16)
+  const a = await getPokemonTypes()
 
   return (
     <AnimatableMain
@@ -21,6 +23,10 @@ export default async function DashboardPage({ searchParams }: DashboardPageProps
         duration: 0.5
       }}
       className='mt-14'>
+      <img
+        src='/pocemonz.svg'
+        className='fixed top-0 -z-40 h-screen w-screen scale-150 blur-2xl md:-left-[400px]'
+      />
       <PokemonList pokemons={pokemons} />
     </AnimatableMain>
   )
